f1c6550943ef02a357598b3d8957f2aa9bd0c219
SDownes <sammdownes@outlook.com>
2018-09-27 12:52:06 +0000

    Created add_comment view to enable comments to be added to tickets using a POST request.

--- 05_milestone-five/tickets/views.py
+++ 05_milestone-five/tickets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.utils import timezone
 from .forms import TicketForm
-from .models import Ticket
+from .models import Ticket, Comment
 
 # Create your views here.
 
@@ -11,16 +12,29 @@ def return_tickets(request):
     
     # All created tickets 
     tickets = Ticket.objects.all()
+    comments = Comment.objects.all()
     
     if request.method == "POST":
         ticket_form = TicketForm(request.POST)
         
         if ticket_form.is_valid():
             ticket_form.save()
-            return redirect(reverse('tickets'))
+            return redirect(reverse('return_tickets'))
     
     else:
         ticket_form = TicketForm()
     
-    return render(request, "tickets.html", { "form": ticket_form , "tickets": tickets } )
+    return render(request, "tickets.html", { "form": ticket_form , "tickets": tickets , "comments": comments } )
     
+    
+def add_comment(request, id):
+    """
+    Add a comment posted in tickets.html to the associated ticket using its id
+    """
+    current_ticket = Ticket.objects.get(pk = id)
+    
+    if request.method == "POST":
+        # Create new Comment instance using the POST request and storing value in the current_ticket variable
+        comment = Comment.objects.create(comment = request.POST['comment'], ticket = current_ticket)
+        
+    return redirect(reverse('return_tickets') )
