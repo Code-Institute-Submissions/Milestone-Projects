2af71e7529ec40612bd6351f3ec689359824a21a
SDownes <sammdownes@outlook.com>
2018-09-27 12:50:02 +0000

    Created comment model to store comments for tickets.

--- 05_milestone-five/tickets/models.py
+++ 05_milestone-five/tickets/models.py
@@ -4,10 +4,19 @@ from django.utils import timezone
 
 # Create your models here.
 
+# Issue choices to be passed to Ticket model
 ISSUE_TYPE = (
     ('BUG', 'Bug'),
     ('FEATURE', 'Feature')
     )
+    
+# Status to be passed to Ticket model
+STATUS_TYPE = (
+    ('1', 'To Do'),
+    ('2', 'Doing'),
+    ('3', 'Done')
+    )
+
 
 class Ticket(models.Model):
     """
@@ -20,6 +29,18 @@ class Ticket(models.Model):
     date = models.DateField(default = timezone.now)
     upvotes = models.IntegerField(default = 1)
     is_feature = models.BooleanField(default = False)
+    status = models.CharField(max_length = 10, choices = STATUS_TYPE, default = 'To Do')
     
     def __str__(self):
-        return self.name
+        return "{0}-{1}-{2}".format(self.name, self.topic, self.date)
+
+
+class Comment(models.Model):
+    """
+    Allows users to add comments to existing issues
+    """
+    comment = models.TextField()
+    ticket = models.ForeignKey(Ticket, related_name = 'comments')
+
+    def __str__(self):
+        return "{0}-{1}".format(self.comment, self.ticket)
