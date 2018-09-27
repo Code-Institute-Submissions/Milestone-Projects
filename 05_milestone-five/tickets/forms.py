43ce1a967867f5ab197e425f76bb86bfeef0b86e
SDownes <sammdownes@outlook.com>
2018-09-27 12:58:38 +0000

    Updated form element to exclude additional fields.

--- 05_milestone-five/tickets/forms.py
+++ 05_milestone-five/tickets/forms.py
@@ -1,13 +1,14 @@
-from django.forms import forms, ModelForm
+from django import forms
 from .models import Ticket
 
-class TicketForm(ModelForm):
+class TicketForm(forms.ModelForm):
     """
     Using the Ticket model to create a form to populate its attributes
     """
     class Meta:
         model = Ticket
-        # Exclude this field as the user will only be able to upvote an existing Ticket
-        exclude = ['upvotes']
+        # Exclude these fields as the user will only be able to upvote an existing Ticket
+        # and the status of a new ticket will automatically be 'To Do'
+        exclude = ['upvotes', 'status', 'comments']
     
         
