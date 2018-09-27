3b4c875a15cf3f13209ad642057b1fd326888de9
SDownes <sammdownes@outlook.com>
2018-09-27 12:53:46 +0000

    Added URL for add_comment view.

--- 05_milestone-five/tickets/urls.py
+++ 05_milestone-five/tickets/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import return_tickets
+from .views import return_tickets, add_comment
 
 urlpatterns = [
-    url(r'^$', return_tickets, name = 'return_tickets')
+    url(r'^$', return_tickets, name = 'return_tickets'),
+    url(r'^comment/(?P<id>\d+)', add_comment, name = 'add_comment'),
     ]
