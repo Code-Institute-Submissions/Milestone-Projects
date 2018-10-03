00d4524884d38c545db4d5fb2fc6fb155c3b1c99
SDownes <sammdownes@outlook.com>
2018-10-03 13:18:44 +0000

    Added URL for Stripe payment feature.

--- 05_milestone-five/tickets/urls.py
+++ 05_milestone-five/tickets/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import return_tickets, add_comment
+from .views import return_tickets, add_comment, make_payment
 
 urlpatterns = [
     url(r'^$', return_tickets, name = 'return_tickets'),
     url(r'^comment/(?P<id>\d+)', add_comment, name = 'add_comment'),
+    url(r'^payment/(?P<id>\d+)', make_payment, name = 'make_payment'),
     ]
